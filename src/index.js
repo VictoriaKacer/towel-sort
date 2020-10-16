12d0334e12d0c2bced9c37247dacd218e777068b
VictoriaKacer <kacer1703@gmail.com>
2020-10-16 15:56:14 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
-
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+    if (!matrix || matrix.length == 0) return [];
+    let result = [];
+    if (matrix[1].length > 0 && !matrix[2]) {
+        result = [].concat(matrix[0], matrix[1].reverse());
+    } else if (matrix[2].length > 0 && !matrix[3]) {
+        result = [].concat(matrix[0], matrix[1].reverse(), matrix[2]);
+    } else if (matrix[3].length > 0 && !matrix[4]) {
+        result = [].concat(matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse());
+    } else {
+        return result;
+    }
+    return result;
 }
